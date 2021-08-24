caee0046f1cb6b331a03256b2a9f00dd7c61cde5
mdaffonso <mdaffonso@gmail.com>
2021-08-24 13:26:35 -0300

    lista de felinos: correções textuais

--- felinos/app.js
+++ felinos/app.js
@@ -3,37 +3,37 @@ const data = [
   {
     title: 'O tigre',
     imgUrl: './img/img-tigre.jpg',
-    description: 'O Tigre (Panthera tigris) é umadas espécies dasubfamília dos panterinos (família Felidae) pertencenteao gêneroPanthera. Só é encontrado no continente asiático;É um predadorcarnívoro e é a maior espécie de felino do mundo,junto com o leão,podendo ambos atingir um tamanho comparável ao dosmaiores fósseis defelinos.',
+    description: 'O Tigre (Panthera tigris) é uma das espécies da subfamília dos panterinos (família Felidae) pertencente ao gênero Panthera. Só é encontrado no continente asiático; é um predador carnívoro e é a maior espécie de felino do mundo, junto com o leão, podendo ambos atingir um tamanho comparável ao dos maiores fósseis de felinos.',
     createdAt: '2021-06-01T23:12:11.837Z'
   }, 
   {
     title: 'O leão',
     imgUrl: './img/img-leao.png',
-    description: 'O leão (Panthera leo) é um mamíferocarnívoro dafamília dos felídeos que é uma das cinco espéciesdo gênero Panthera.Leões selvagens vivem em populações cada vez maisdispersas efragmentadas na África Subsaariana (com exceção dasregiões de selva dacosta atlântica e da Bacia do Congo) e em uma pequenaárea do noroesteda Índia',
+    description: 'O leão (Panthera leo) é um mamífero carnívoro da família dos felídeos que é uma das cinco espécies do gênero Panthera. Leões selvagens vivem em populações cada vez mais dispersas e fragmentadas na África Subsaariana (com exceção das regiões de selva da costa atlântica e da Bacia do Congo) e em uma pequena área do noroeste da Índia',
     createdAt: '2021-05-05T23:12:11.837Z'
   }, 
   {
     title: 'O leopardo',
     imgUrl: './img/img-leopardo.jpg',
-    description: 'O leopardo (Panthera pardus) é ummamífero carnívoroda família dos felídeos. Como três dos outros gatosdo gênero Panthera:o leão, o tigre e a onça, eles se caracterizam poruma modificação noosso hióide que lhes permite rugir. É também conhecidacomo panteramarrom e, quando tem a pelagem totalmente escura,como pantera negra(melânica).',
+    description: 'O leopardo (Panthera pardus) é um mamífero carnívoro da família dos felídeos. Como três dos outros gatos do gênero Panthera: o leão, o tigre e a onça, eles se caracterizam por uma modificação no osso hióide que lhes permite rugir. É também conhecida como pantera marrom e, quando tem a pelagem totalmente escura, como pantera negra (melânica).',
     createdAt: '2021-05-05T23:12:11.837Z'
   }, 
   {
     title: 'A pantera negra',
     imgUrl: './img/img-pantera-negra.png',
-    description: 'A pantera negra é uma variação negra(melanismo) devárias espécies de grandes felinos, principalmenteo leopardo (Pantherapardus) e a onça pintada (Panthera onca). Mas deve-seressaltar que nãoé uma espécie nova, nem mesmo uma subespécie, é simplesmenteumavariação negra desses animais.',
+    description: 'A pantera negra é uma variação negra (melanismo) de várias espécies de grandes felinos, principalmenteo leopardo (Panthera pardus) e a onça pintada (Panthera onca). Mas deve-se ressaltar que não é uma espécie nova, nem mesmo uma subespécie, é simplesmente uma variação negra desses animais.',
     createdAt: '2021-04-09T23:12:11.837Z'
   }, 
   {
     title: 'O jaguar',
     imgUrl: './img/img-jaguar.jpg',
-    description: 'O jaguar, jaguar ou yaguareté (Pantheraonca) é umfelino carnívoro da subfamília dos Panterinos e gêneroPanthera. É aúnica das cinco espécies atuais desse gênero encontradasna América. Étambém o maior felino da América e o terceiro maiordo mundo, depois dotigre (Panthera tigris) e do leão (Panthera leo).',
+    description: 'O jaguar, jaguar ou yaguareté (Panthera onca) é um felino carnívoro da subfamília dos Panterinos e gênero Panthera. É a única das cinco espécies atuais desse gênero encontradas na América. É também o maior felino da América e o terceiro maior do mundo, depois do tigre (Panthera tigris) e do leão (Panthera leo).',
     createdAt: '2021-05-18T23:12:11.837Z'
   }, 
   {
     title: 'O guepardo',
     imgUrl: './img/img-guepardo.png',
-    description: 'A chita (Acinonyx jubatus) 1 é ummembro atípico dafamília dos felídeos. É o único representante vivodo gênero Acinonyx.Ele caça graças à sua visão e grande velocidade. Éo animal terrestremais rápido, atingindo velocidade máxima de 115 km/ h em corridas deaté quatrocentos ou quinhentos metros.',
+    description: 'A chita (Acinonyx jubatus) é um membro atípico da família dos felídeos. É o único representante vivo do gênero Acinonyx. Ele caça graças à sua visão e grande velocidade. É o animal terrestre mais rápido, atingindo velocidade máxima de 115 km/h em corridas de até quatrocentos ou quinhentos metros.',
     createdAt: '2021-05-22T23:12:11.837Z'
   }
 ]
